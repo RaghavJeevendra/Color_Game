3c00ff0924e0d8f93d731eb6a7fe807d90597fe9
RaghavJeevendra <raghavj0505@gmail.com>
2020-02-19 18:13:02 -0700

    added js

---
+++ color.js
@@ -0,0 +1,29 @@
+var colors = [ 'rgb(0, 0, 255)',
+			   'rgb(0, 255, 255)',
+			   'rgb(0, 255, 0)',
+			   'rgb(255, 0, 255)',
+			   'rgb(255, 0, 0)',
+			   'rgb(255, 255, 0)'
+]
+
+var squares = document.querySelectorAll('.square')
+var pickedColor = colors[3]
+var colordisplay = document.getElementById('colordisplay')
+
+colordisplay.textContent = pickedColor
+
+for(var i =0 ; i < squares.length ; i++){
+	squares[i].style.background = colors[i];
+
+	squares[i].addEventListener('click', function(){
+		var clickedColor = this.style.background
+
+		if(clickedColor === pickedColor){
+			alert('correct!')
+		}else{
+			alert('WRONG!!')
+		}
+	})
+
+
+}
